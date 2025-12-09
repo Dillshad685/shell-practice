272f1a3b6da141504329c21d69178a8ae312f3fb
Dillshad685 <dillshadb2@gmail.com>
2025-12-09 18:34:03 +0530

    shell-commit

--- backuplog.sh
+++ backuplog.sh
@@ -10,7 +10,7 @@ DEST_DIR=$2
 DAYS=${3:-14}   #considers 14 if $3 is not passed
 
 LOGS_FOLDER=/var/log/shell-script
-SCRIPT_NAME=$( echo $0 | cut -d "." f1 )
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
@@ -55,12 +55,13 @@ fi
 
 FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)
 
-if [ ! -z $"{FILES}" ]; then # -z checks if string is empty
+if [ ! -z "${FILES}" ] ; then 
+# -z checks if string is empty
     echo "files found: $FILES"
     TIMESTAMP=$(date +%F-%H-%M)
     ZIP_FILE_NAME="$DEST_DIR/app-logs-$TIMESTAMP.zip"
-    echo "Zip file is " $ZIP_FILE_NAME"
-    find $SOURCE_DIR  -name "*.log" -type -f -mtime +$DAYS | zip -@ -j "$ZIP_FILE_NAME"
+    echo "Zip file is $ZIP_FILE_NAME"
+    find $SOURCE_DIR  -name "*.log" -type f -mtime +$DAYS | zip -@ -j "$ZIP_FILE_NAME"
     echo "$ZIP_FILE_NAME"
 
 #Check if zipped or not
@@ -68,7 +69,7 @@ if [ ! -z $"{FILES}" ]; then # -z checks if string is empty
     if [ -f $ZIP_FILE_NAME ]; then
         echo "files are zipped"
 
-        while IFS = read -r filepath
+        while IFS= read -r filepath
         do
             echo "Deleting files : $filepath"
             rm -rf $filepath
