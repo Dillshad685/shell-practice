ebccd91dd1c2262bbefd9a9a8a5d23867ed2c0d0
Dillshad685 <dillshadb2@gmail.com>
2025-12-10 00:03:27 +0530

    shell-commit

--- cpu-usage.sh
+++ cpu-usage.sh
@@ -8,7 +8,7 @@ MESSAGE=""
 
 while IFS= read -r line
 do
-    USAGE=$(echo $line | awk '{print$6}' | cut -d "%" -f1)
+    USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
     PARTITION=$(echo $line | awk '{print$7}')
     if [ $USAGE -ge $DISK_THRESHOLD ]; then
         #MESSAGE+="High disk utilization on $PARTITON : $USAGE % </br>"
