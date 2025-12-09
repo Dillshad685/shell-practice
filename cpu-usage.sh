29d07d6dd8576bf33b0198252a33df9bc230a294
Dillshad685 <dillshadb2@gmail.com>
2025-12-09 23:56:03 +0530

    shell-commit

--- cpu-usage.sh
+++ cpu-usage.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
 DISK_USAGE=$(df -hT | grep -v filesystem)
+DISK_THRESHOLD=2
+IP_address=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+MESSAGE=""
+
 
 while IFS= read -r line
 do
-    echo "Line: $line"
+    USAGE=$(echo $line | awk '{print$6}' | cut -d "%" -f1)
+    PARTITON=$(echo $line | awk '{print$7}')
+    if [ $USAGE -ge $DISK_THRESHOLD ]; then
+        MESSAGE+="High disk utilization on $PARTITON : $USAGE % </br>"
+    fi
 done <<<$DISK_USAGE
+
+echo -e "Message body: $MESSAGE"
+
+sh mail.sh "dillu945@gmail.com" "High disk usage alert" "High disk usage" "$MESSAGE" "$IP_address" "Devops team"
+
+# TO_ADDRESS=$1
+# SUBJECT=$2
+# ALERT_TYPE=$3
+# MESSAGE_BODY=$4
+# IP_ADDRESS=$5
+
+
 
